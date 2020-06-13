85a6cb9fb1b6bc950108420b5ab8e78f8ddaa83c
arsyfpro <62231022+arsyfpro@users.noreply.github.com>
2020-06-13 10:22:33 +0700

    Membuat Input nama

--- tubes.cpp
+++ tubes.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main(){
+  string nama;
+  
   cout << "Hello World!" << endl;
+  cout << "Masukkan nama Anda: "; cin >> nama;
+  cout << "Hallo " << nama << " selamat datang!" << endl;
   
   return 0;
 }
