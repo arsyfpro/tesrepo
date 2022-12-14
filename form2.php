a7c66485141c020694e427377c6841ccdaebcdde
arsyfpro <udonorewakoneko@gmail.com>
2022-12-15 00:04:36 +0700

    add form2.php

---
+++ form2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GET & POST Method</title>
+</head>
+<body>
+	<form action="catch_post.php" method="POST">
+        Nama: <input type="text" name="nama"/><br>
+        Umur: <input type="number" name="umur"/><br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>
