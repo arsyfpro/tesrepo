86dd0bc7868e5ae29095891b698875577f95785d
arsyfpro <udonorewakoneko@gmail.com>
2022-12-15 00:03:05 +0700

    ubah form_input.php

--- form_input.php
+++ form_input.php
@@ -5,8 +5,8 @@
 </head>
 <body>
 	<form action="catch_post.php" method="POST">
-        Nama: <input type="text" name="nama"><br>
-        Umur: <input type="number" name="umur"><br>
+        Nama: <input type="text" name="nama"/><br>
+        Umur: <input type="number" name="umur"/><br>
         <button type="submit" name="submit">Submit</button>
     </form>
 </body>
